b17cb7364287a52ace34ed90702753383f76e600
wkasel <william@jumpfox.com>
2012-11-26 21:55:46 -0800

    Fixed Port

--- web.js
+++ web.js
@@ -33,8 +33,6 @@ app.get('/', function(req, res) {
 });
 
 var server = http.createServer(app).listen(app.get('port'), function(){
-  
   console.log("Express server listening on port " + app.get('port'));
 });
-
-server.listen(3000);
+server.listen(app.get('port'));
